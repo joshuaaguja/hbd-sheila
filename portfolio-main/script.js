24fa6de045002c84b15194890083ba447ed77fb0
joshuaaguja <153116025+joshuaaguja@users.noreply.github.com>
2024-09-03 17:37:15 +0800

    Add files via upload

---
+++ portfolio-main/script.js
@@ -0,0 +1,92 @@
+function addRecommendation() {
+  // Get the message of the new recommendation
+  let recommendation = document.getElementById("new_recommendation");
+  // If the user has left a recommendation, display a pop-up
+  if (recommendation.value != null && recommendation.value.trim() != "") {
+    console.log("New recommendation added");
+    //Call showPopup here
+
+    // Create a new 'recommendation' element and set it's value to the user's message
+    var element = document.createElement("div");
+    element.setAttribute("class","recommendation");
+    element.innerHTML = "\<span\>&#8220;\</span\>" + recommendation.value + "\<span\>&#8221;\</span\>";
+    // Add this element to the end of the list of recommendations
+    document.getElementById("all_recommendations").appendChild(element); 
+    
+    // Reset the value of the textarea
+    recommendation.value = "";
+  }
+  showPopup(true);
+}
+
+function showPopup(bool) {
+  if (bool) {
+    document.getElementById('popup').style.visibility = 'visible'
+  } else {
+    document.getElementById('popup').style.visibility = 'hidden'
+  }
+}
+console.log("text")
+document.addEventListener("DOMContentLoaded", function() {
+const coords = { x: 0, y: 0 };
+const circles = document.querySelectorAll(".circle");
+
+const colors = [
+  "#ff66b2",
+  "#ff5cae",
+  "#ff52aa",
+  "#ff48a6",
+  "#ff3ea2",
+  "#ff349e",
+  "#ff2a9a",
+  "#ff2096",
+  "#ff1692",
+  "#ff0c8e",
+  "#f6028a",
+  "#eb0086",
+  "#e00082",
+  "#d5007e",
+  "#ca007a",
+  "#bf0076",
+  "#b50072",
+  "#a8006e",
+  "#9d006a",
+  "#920066",
+  "#870062",
+  "#7c005e"
+];
+
+console.log(circles);
+circles.forEach(function (circle, index) {
+  circle.x = 0;
+  circle.y = 0;
+  circle.style.backgroundColor = colors[index % colors.length];
+});
+
+window.addEventListener("mousemove", function(e){
+  coords.x = e.clientX;
+  coords.y = e.clientY;
+});
+
+function animateCircles() {
+  let x = coords.x;
+  let y = coords.y;
+  circles.forEach(function (circle, index) {
+    circle.style.left = x - 12 + "px";
+    circle.style.top = y - 12 + "px";
+    
+    circle.style.scale = (circles.length - index) / circles.length;
+    
+    circle.x = x;
+    circle.y = y;
+
+    const nextCircle = circles[index + 1] || circles[0];
+    x += (nextCircle.x - x) * 0.3;
+    y += (nextCircle.y - y) * 0.3;
+  });
+ 
+  requestAnimationFrame(animateCircles);
+}
+
+animateCircles();
+});
